95548c31033dc8a67aaae131ba22662b15230b14
jerryliao26 <jerryliao26@gmail.com>
2017-03-12 17:57:59 +0800

    first commit my code into git

---
+++ src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+
+import { AppComponent } from './app.component';
+
+import { MaterialModule } from '@angular/material';
+import "hammerjs";
+import { IndexComponent } from './index/index.component';
+import { HeaderComponent } from './global/header/header.component';
+import { FooterComponent } from './global/footer/footer.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    IndexComponent,
+    HeaderComponent,
+    FooterComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    MaterialModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
